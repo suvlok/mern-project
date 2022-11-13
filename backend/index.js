26f1cc6866168082d84f4f11666348b64d6b86f1
suvlok <suvanloku@hotmail.com>
2022-11-13 17:22:15 -0500

    Connected to the database in mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,26 +1,26 @@
 import app from "./server.js"
-import mongodb, { MONGO_CLIENT_EVENTS } from "mongodb"
+import mongodb from "mongodb"
 import dotenv from "dotenv"
+import RestaurantsDAO from "./dao/restaurantsDAO.js"
 dotenv.config()
 const MongoClient = mongodb.MongoClient
 
 const port = process.env.PORT || 8000
 
 MongoClient.connect(
-    process.env.RESTREVIEW_DB_URI,
- {
-    poolSize : 50,
-    wtimeout: 2500,
-    useNewUrlParser: true}
-)
-.catch(err => {
-    console.error(err.stack)
-    process.exit(1)
-})
-.then(async client => {
-    app.listen(port, () => {
-        console.log('listening on port ${port}')
+    process.env.RESTREVIEWS_DB_URI,
+    {
+        maxPoolSize : 50,
+        wtimeoutMS: 2500,
+        useNewUrlParser: true}
+    )
+    .catch(err => {
+        console.error(err.stack)
+        process.exit(1)
     })
-})
-
-//21:42
+    .then(async client => {
+        await RestaurantsDAO.injectDB(client)
+        app.listen(port, () => {
+            console.log(`listening on port ${port}`)
+    })
+    })
